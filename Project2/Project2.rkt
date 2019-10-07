c04f5797e4bae8222dc5238674d5b59eae48ae10
Jared Diehl <jareddiehl96@gmail.com>
2019-10-06 22:20:35 -0500

    Cleaning up

--- Project2/Project2.rkt
+++ Project2/Project2.rkt
@@ -7,44 +7,43 @@
 (define pi 3.1416)
 
 #|
-(define (my_area flag radius) ; create a function named my_area1 with parameters flag (integer) and radius (real)
+(define (my_area flag radius)
   (cond
-    [(< radius 0) #f] ; if the radius is less than 0, then return false
-    [(= flag 1) (* pi (* radius radius))] ; if the flag is equal to 1, then return the area of a circle using radius
-    [(= flag 2) (* (/ 4 3) pi (* radius radius radius))] ; if the flag is equal to 2, the return the volume of a sphere using radius
-    [else #f])) ; if the flag is not equal to 1 or 2, then return false
+    [(< radius 0) #f]
+    [(= flag 1) (* pi (* radius radius))]
+    [(= flag 2) (* (/ 4 3) pi (* radius radius radius))]
+    [else #f]))
 |#
 
 (define (my_area flag radius)
-  (if (>= radius 0)
+  (if (< radius 0)
+      #f
       (if (= flag 1)
           (* pi (* radius radius))
           (if (= flag 2)
               (* (/ 4 3) pi (* radius radius radius))
-              #f))
-      #f))
+              #f))))
 
 (define (rem_second list)
-  (if (>= (length list) 2)
+  (if (= (length list) 2)
       (cons (car list) (cdr (cdr list)))
       '()))
 
 (define (my_union a b)
   (cond
-    [(not (list? a)) (error "Error: The first parameter is not a list!")]
-    [(not (list? b)) (error "Error: The second parameter is not a list!")]
     [(null? b) a]
     [(member (car b) a) (my_union a (cdr b))]
     [else (my_union (cons (car b) a) (cdr b))]))
 
+;; (my_union '(1 2) '(1 2)) WORK
+;; (my_union '(1 2 1) '(1 2 1)) NOT WORK
+
 (define (my_delete atom list)
   (cond
-    [(not (atom? atom)) (error "Error: The first parameter is not an atom!")]
-    [(not (list? list)) (error "Error: The second parameter is not a list!")]
     [(null? list) list]
-    [(list? (first list)) (cons (my_delete atom (first list)) (my_delete atom (rest list)))]
-    [(equal? atom (first list)) (my_delete atom (rest list))]
-    [else (cons (first list) (my_delete atom (rest list)))]))
+    [(list? (car list)) (cons (my_delete atom (car list)) (my_delete atom (cdr list)))]
+    [(equal? atom (car list)) (my_delete atom (cdr list))]
+    [else (cons (car list) (my_delete atom (cdr list)))]))
 
 ;; (my_delete 'a '(a))
 ;; (my_delete 1 '(1))
